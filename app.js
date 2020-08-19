f2ee12ac44ef0dba9304feb3b5798f52b08108eb
Heecheon <heecheon92@gmail.com>
2020-08-19 10:21:41 +0900

    added local login / sns login

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const findOrCreate = require("mongoose-findorcreate");
 const https = require("https");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const LocalStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const NaverStrategy = require("passport-naver").Strategy;
 const methodOverride = require("method-override");
 const _ = require("lodash");
 
@@ -23,6 +30,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
+app.use(session({
+  secret: "warning suppression",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const dbPath = "내 mongodb atlas cluster 경로";
 mongoose.connect(dbPath, {
@@ -42,10 +56,85 @@ const postsSchema = {
   content: String
 };
 
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  googleId: String,
+  naverId: String
+});
+/* Passport-local-mongoose plugin with 
+  an option to handle email field instead of
+  the default "username".
+*/
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: "email",
+  errorMessages: {
+    MissingPasswordError: 'No password was given',
+    AttemptTooSoonError: 'Account is currently locked. Try again later',
+    TooManyAttemptsError: 'Account locked due to too many failed login attempts',
+    NoSaltValueStoredError: 'Authentication not possible. No salt value stored',
+    IncorrectPasswordError: 'Password or username are incorrect',
+    IncorrectUsernameError: 'Password or username are incorrect',
+    MissingUsernameError: 'No username was given',
+    UserExistsError: 'A user with the given username is already registered'
+  }
+});
+userSchema.plugin(findOrCreate);
 const Post = mongoose.model("Post", postsSchema);
 const defaultPost = new Post({
   title: "Placeholder Title",
   content: "Placeholder Content"
+});
+
+passport.use(User.createStrategy());
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID_GOOGLE,
+  clientSecret: process.env.CLIENT_SECRET_GOOGLE,
+  callbackURL: process.env.GOOGLE_CALLBACK,
+  // Prevent Google+ deprecation warning with userProfileURL field.
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+}, function (accessToken, refreshToken, profile, cb) {
+  console.log(profile);
+  User.findOrCreate({
+    googleId: profile.id
+  }, function (err, user) {
+    return cb(err, user);
+  })
+}));
+
+passport.use(new NaverStrategy({
+  clientID: process.env.CLIENT_ID_NAVER,
+  clientSecret: process.env.CLIENT_SECRET_NAVER,
+  callbackURL: process.env.NAVER_CALLBACK
+}, function (accessToken, refreshToken, profile, cb) {
+  console.log(profile);
+  User.findOrCreate({
+    naverId: profile.displayName
+  }, function (err, user) {
+    return cb(err, user);
+  })
+}));
+
+/* Global setting for dynamic navbar
+  Changing "sign-in" to "sign-out" based on
+  log-in status.
+*/
+app.use((req, res, next) => {
+  res.locals.loginStat = req.isAuthenticated();
+  if (!res.locals.loginStat) {
+    res.locals.failureReason = "";
+  }
+  next();
 });
 
 app.get("/", function (req, res) {
@@ -84,6 +173,88 @@ app.get("/", function (req, res) {
   })
 
 })
+app.get("/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile"]
+  })
+);
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", {
+    failureRedirect: "/sign-in"
+  }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
+app.get("/auth/naver",
+  passport.authenticate("naver", null), (req, res) => {
+    console.log("네이버 로그인 인증경로 진입");
+  }
+);
+
+app.get("/auth/naver/secrets", passport.authenticate("naver", {
+  failureRedirect: "/sign-in"
+}), function (req, res) {
+  res.redirect("/");
+});
+
+app.get("/sign-in", function (req, res) {
+  res.render("sign-in", {
+    failureReason: loginFailMsg
+  });
+});
+
+app.post("/sign-in", function (req, res) {
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+  req.login(user, function (err) {
+    if (err) {
+      loginFailMsg = err;
+      console.log(err);
+      return res.redirect("/sign-in");
+    } else {
+      passport.authenticate("local", {
+        failureRedirect: "/loginFail",
+        failureFlash: false
+      })(req, res, function () {
+        res.redirect("/");
+      })
+    }
+  })
+});
+
+app.get("/loginFail", function (req, res){
+  res.render("sign-in", {
+    failureReason: "Wrong username or password."
+  })
+});
+
+app.post("/register", function (req, res) {
+  User.register(new User({
+    email: req.body.email,
+    username: req.body.name
+  }), req.body.password, function (err, user) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Authentication initilized");
+      console.log("User: " + user);
+      // user.name = req.body.name;
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/");
+      });
+    }
+  });
+});
+
+app.get("/sign-out", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
 app.get("/about", function (req, res) {
   res.render("about", {
